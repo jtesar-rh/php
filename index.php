1a2d62603a803f9e00986f49ba0c386b612f4e00
Jirka Tesar <jtesar@redhat.com>
2024-06-02 15:23:09 -0400

    dd

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'backend:3306';
+$host = $_ENV['BACKEND_IP'];
 $user = 'jtesar';
 $pass = 'redhat';
 $dbname = 'db';
@@ -7,19 +7,16 @@ $conn = mysqli_connect($host, $user, $pass,$dbname);
 if(!$conn){
   die('Could not connect: '.mysqli_connect_error());
 }
-echo 'Connected successfully<br/>';
-
 $sql = 'SELECT * FROM names';
 $retval=mysqli_query($conn, $sql);
 
 if(mysqli_num_rows($retval) > 0){
  while($row = mysqli_fetch_assoc($retval)){
-    echo "$row['name']";
+    echo "{$row['name']}";
     echo "\n";
  } //end of while
 }else{
 echo "0 results";
 }
 mysqli_close($conn);
-?>
 ?>
